ffe081400016e7867163b48f8e5ad44cf2914dcb
guowentao <gwem.win@gmail.com>
2019-08-14 15:58:00 +0800

    'history'

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,7 @@ import Detail from '@/pages/detail/detail'
 Vue.use(Router);
 
 export default new Router({
-    mode:'history',
+    // mode:'history',
     routes: [{
         path:'/',
         name:'Home',
